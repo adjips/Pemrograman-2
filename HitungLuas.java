86371ba92d2b19ee63920f46ebfec01061e6520f
Adji Pangestu <adji.pangestu1407@gmail.com>
2019-01-25 14:49:07 +0700

    Tambahan method Hitung Luas

---
+++ HitungLuas.java
@@ -0,0 +1,46 @@
+package tugas1;
+
+import java.util.Scanner;
+
+public class HitungLuas {
+    public static void LuasPersegiPanjang() {
+        int panjang;
+        int lebar;
+        int luas;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan panjang(cm) : ");
+        panjang = input.nextInt();
+
+        System.out.print("Masukan Lebar(cm)   : ");
+        lebar = input.nextInt();
+
+        luas = panjang * lebar;
+
+        System.out.println("Luas Persegi dengan Panjang " + panjang + " cm dan lebar " + lebar + " cm = " + luas + " cm");
+        System.out.println("");
+    }
+    public static void LuasLingkaran() {
+        double jarijari;
+        double luas;
+        double phi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan jari-jari(cm) : ");
+        jarijari = input.nextInt();
+
+        luas = 2 * jarijari * phi;
+
+        System.out.print("Luas lingkaran dengan jari-jari " + jarijari + " = " + luas + " cm");
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Menghitung Luas Persegi Panjang!");
+        LuasPersegiPanjang();
+
+        System.out.println("Menghitung Luas Lingkaran!");
+        LuasLingkaran();
+    }
+
+}
